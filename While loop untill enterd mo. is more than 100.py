1c63e8d3914a4c57840ba14c14efbeea44e3b653
AmmarRamlawala <73690569+AmmarRamlawala@users.noreply.github.com>
2020-11-01 13:29:01 +0530

    Add files via upload

---
+++ While loop untill enterd mo. is more than 100.py
@@ -0,0 +1,15 @@
+print("enter a number")
+
+while(True):
+    n = int(input())
+    if (n > 100):
+        print("Your number is greater than 100")
+        break
+        
+
+    else:
+        print("Try again")
+        continue
+       
+
+        
